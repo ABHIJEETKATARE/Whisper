e9ad860d14569fd373df8f03f40077f1c3ddd718
ABHIJEETKATARE <abhijeetkatare19274@gmail.com>
2024-11-09 13:45:28 +0530

    new file mobile_chat_screen is added

---
+++ lib/screens/mobile_chat_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:whisper/colors.dart';
+import 'package:whisper/info.dart';
+import 'package:whisper/widgets/chat_list.dart';
+
+class MobileChatScreen extends StatelessWidget {
+  const MobileChatScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: appBarColor,
+        title: Text(
+          info[0]['name'].toString(),
+        ),
+        centerTitle: false,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.video_call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.call),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.more_vert),
+          ),
+        ],
+      ),
+    );
+  }
+}
